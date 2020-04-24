b5d85a533fbde586261480eb71caf6e70d9ee945
Willian Souza <willian.souza@fbssistemas.com.br>
2020-04-24 17:07:59 -0300

    Initial Login Api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,14 @@ class UserController extends Controller
     {
         return User::all();
     }
+
+    public function login()
+    {
+        $email = request()->input('email');
+        $password = request()->input('password');
+        if ($email != 'willian.souza@fbssistemas.com.br' || $password != '123456') {
+            return response()->json(['message' => 'E-mail ou Senha Inválido!'], 400);
+        }
+        return response()->json(['token' => 'PwdTnhZFwLWFjA6HRL/J7oMl0WmybNbfcOSZZ0OfR80=']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/login', 'UserController@login');
+
 Route::get('/users', 'UserController@index');
 Route::resource('customers', 'CustomerController');
 Route::get('/products', 'ProductController@index');
